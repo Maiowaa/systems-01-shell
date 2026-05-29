2f28468a377beb649913832e695b1f6d8272e364
Kushagar Sood <soodkushagar387@gmail.com>
2026-05-29 13:09:51 +0530

    Handle empty input safely

--- src/main.c
+++ src/main.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 int main()
 {
-    printf("systems-01-shell\n");
+    char input[1024];
+    
+
+    while (1)
+    {
+        printf("myshell> ");
+
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            break;
+        }
+        input[strcspn(input, "\n")] = '\0';
+        char *token = strtok(input, " ");
+        char *argv[64];
+        int argc = 0;
+        while (token != NULL && argc < (int)(sizeof(argv) / sizeof(argv[0]) - 1))
+        {
+            argv[argc++] = token;
+            token = strtok(NULL, " ");
+        }
+        argv[argc] = NULL;
+        if (argc == 0)
+        {
+            continue;
+        }
+        
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            perror("fork");
+            continue;
+        }
+        if (pid == 0)
+        {
+            if (execvp(argv[0], argv) < 0)
+            {
+                perror("execvp");
+                return 1;
+            }
+            
+        }
+        else
+        {
+            waitpid(pid, NULL, 0);
+            
+        }
+        if (strcmp(argv[0], "exit") == 0)
+        {
+            break;
+        }
+        
+    }
+
     return 0;
 }
