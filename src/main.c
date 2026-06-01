5061b0a2f7c65098c70f9e4ab21046e6f80e81db
Kushagar Sood <soodkushagar387@gmail.com>
2026-06-01 11:28:57 +0530

    Implement input and output redirection

--- src/main.c
+++ src/main.c
@@ -4,11 +4,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #define MAX_ARGS 64
 
 int parse_input(char *input, char *argv[]);
 int execute_builtin(char *argv[], int argc);
-void execute_external(char *argv[]);
+void execute_external(char *argv[], int argc);
 
 
 int main()
@@ -34,7 +35,7 @@ int main()
         if (execute_builtin(argv, argc) == 1)
             continue;
         
-        execute_external(argv);
+        execute_external(argv, argc);
     }
     
     return 0;
@@ -44,12 +45,13 @@ int main()
 int parse_input(char *input, char *argv[])
 {
     // Current strtok logic
-    char *token = strtok(input, " ");
+    char *token = strtok(input, " \t");
     int argc = 0;
+    
     while (token != NULL && argc < MAX_ARGS - 1)
     {
         argv[argc++] = token;
-        token = strtok(NULL, " ");
+        token = strtok(NULL, " \t");
     }
     argv[argc] = NULL;
     return argc;  // return argc so caller knows how many args
@@ -90,24 +92,106 @@ int execute_builtin(char *argv[], int argc)
 }
 
 // Execute external command via fork/exec
-void execute_external(char *argv[])
+void execute_external(char *argv[], int argc)
 {
+    char *input_file = NULL;
+    char *output_file = NULL;
+    int new_argc = argc;
+
+    for (int i = 0; i < new_argc; i++)
+    {
+        if (strcmp(argv[i], ">") == 0)
+        {
+            if (output_file != NULL)
+            {
+                fprintf(stderr, "Multiple output redirections not supported\n");
+                return;
+            }
+            if (i + 1 >= new_argc)
+            {
+                fprintf(stderr, "Missing output file\n");
+                return;
+            }
+            output_file = argv[i + 1];
+            for (int j = i; j + 2 < new_argc; j++)
+                argv[j] = argv[j + 2];
+            new_argc -= 2;
+            argv[new_argc] = NULL;
+            i--;
+            continue;
+        }
+        if (strcmp(argv[i], "<") == 0)
+        {
+            if (input_file != NULL)
+            {
+                fprintf(stderr, "Multiple input redirections not supported\n");
+                return;
+            }
+            if (i + 1 >= new_argc)
+            {
+                fprintf(stderr, "Missing input file\n");
+                return;
+            }
+            input_file = argv[i + 1];
+            for (int j = i; j + 2 < new_argc; j++)
+                argv[j] = argv[j + 2];
+            new_argc -= 2;
+            argv[new_argc] = NULL;
+            i--;
+            continue;
+        }
+    }
+
     pid_t pid = fork();
     if (pid < 0)
     {
         perror("fork");
         return;
     }
-    
-    if (pid == 0)  // child
+
+    if (pid == 0)
     {
+        if (input_file != NULL)
+        {
+            int fd = open(input_file, O_RDONLY);
+            if (fd < 0)
+            {
+                perror("open");
+                exit(1);
+            }
+            if (dup2(fd, STDIN_FILENO) < 0)
+            {
+                perror("dup2");
+                close(fd);
+                exit(1);
+            }
+            close(fd);
+        }
+
+        if (output_file != NULL)
+        {
+            int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0)
+            {
+                perror("open");
+                exit(1);
+            }
+            if (dup2(fd, STDOUT_FILENO) < 0)
+            {
+                perror("dup2");
+                close(fd);
+                exit(1);
+            }
+            close(fd);
+        }
+
         if (execvp(argv[0], argv) < 0)
         {
             perror("execvp");
             exit(1);
         }
     }
-    else  // parent
+    else
     {
         waitpid(pid, NULL, 0);
     }
